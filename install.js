b1d91ceb6a477ef8a870c2cf06ed3a885efa1b81
fxhxyz4 <fxhxyz@proton.me>
2023-05-27 19:33:47 +0300

    small changes

--- install.js
+++ install.js
@@ -1,10 +1,12 @@
+#!/usr/bin/env node
+
 const { execSync } = require("child_process");
 const extensions = require("./extensions.js");
 const colors = require("colors");
 
 const install = (name) => {
-	console.info(`install ext: ${name}`);
 	execSync(`code --install-extension ${name}`);
+	console.info(`install ext: ${name}`);
 };
 
 extensions.forEach(install);
